504dbb5e8c7c198b201896241c8dfb20bf102c32
Alejo boga <alejoboga20@gmail.com>
2021-05-27 11:07:24 -0300

    Adding custom middlewares

--- controller/auth.js
+++ controller/auth.js
@@ -1,24 +1,15 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body;
-  const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
-
-  res.json({ ok: true, msg: 'user created', name, email, password });
+  res.status(201).json({ ok: true, msg: 'user created', name, email, password });
 };
 
 const loginUser = (req, res) => {
   const { email, password } = req.body;
 
-  res.json({ ok: true, msg: 'login', email, password });
+  res.status(200).json({ ok: true, msg: 'login', email, password });
 };
 
 module.exports = { createUser, loginUser };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,28 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createUser, loginUser } = require('../controller/auth');
+const { validateFields } = require('../middlewares/fieldsValidator');
 const router = Router();
 
-router.post('/new', [check('name', 'Name is required').not().isEmpty()], createUser);
+router.post(
+  '/new',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password should be 6 chars long').isLength({ min: 6 }),
+    validateFields
+  ],
+  createUser
+);
 
-router.post('/', loginUser);
+router.post(
+  '/',
+  [
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password should be 6 chars long').isLength({ min: 6 }),
+    validateFields
+  ],
+  loginUser
+);
 
 module.exports = router;
