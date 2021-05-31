8d3c6b592edf88d75390b4416859ee640ab7a4c8
Alejo Boga <alejoboga20@gmail.com>
2021-05-31 14:23:18 -0300

    Creating JWT generator

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,1 +1,18 @@
 const jwt = require('jsonwebtoken');
+
+const generateJWT = (uid, name) => {
+  return new Promise((resolve, reject) => {
+    const payload = { uid, name };
+
+    jwt.sign(
+      payload,
+      process.env.SECREET_JWT_SEED,
+      { expiresIn: '1800s' },
+      (error, token) => {
+        error ? reject('token not generated') : resolve(token);
+      }
+    );
+  });
+};
+
+module.exports = { generateJWT };
