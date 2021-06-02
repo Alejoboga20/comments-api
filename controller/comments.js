ae2a2f1e3599a9243f958567b2eb59cda926f92f
Alejo Boga <alejoboga20@gmail.com>
2021-06-02 09:48:49 -0300

    Creating updateComment controller

--- controller/comments.js
+++ controller/comments.js
@@ -6,8 +6,8 @@ const getComments = async (req, res = response) => {
     const comments = await Comment.find().populate('user', 'name');
 
     return res.json({ ok: true, comments: comments });
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({ ok: false, msg: 'An error has ocured' });
   }
 };
@@ -29,10 +29,34 @@ const createComment = async (req, res = response) => {
 };
 
 const updateComment = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'updateComment'
-  });
+  const commentId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ ok: false, msg: 'Comment not found' });
+    }
+
+    if (comment.user.toString() !== uid) {
+      return res
+        .status(401)
+        .json({ ok: false, msg: 'Not authorized for this change' });
+    }
+
+    const newComment = { ...req.body, user: uid };
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      newComment,
+      { new: true }
+    );
+
+    return res.json({ ok: true, comment: updatedComment });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ ok: false, msg: 'An error has ocured' });
+  }
 };
 
 const deleteComment = async (req, res = response) => {
