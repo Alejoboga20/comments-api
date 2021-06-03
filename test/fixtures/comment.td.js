50281554cea3013dd728e901cdbf930050eb8a4c
Alejo Boga <alejoboga20@gmail.com>
2021-06-03 15:37:56 -0300

    Adding data for comments tests

---
+++ test/fixtures/comment.td.js
@@ -0,0 +1,11 @@
+const newCommentWithValidData = {
+  body: 'This is a test comment'
+};
+
+const testUser = {
+  name: 'testUserForComments',
+  email: 'testuserforcomments@email.com',
+  password: 'TestUser123!'
+};
+
+module.exports = { newCommentWithValidData, testUser };
